f20e72f06f3d70baaed6a57a053131510a4e4219
Petar Sabev <pisabev@gmail.com>
2020-11-23 12:58:41 +0200

    .

--- example/chart/index.dart
+++ example/chart/index.dart
@@ -9,15 +9,16 @@ import '../common/base.dart';
 
 void main() {
     init();
-    //testChart();
+    testChart();
     testChart1();
-    //testCircle();
-  //testBarSmall();
+    testCircle();
+    testBarSmall();
+    testPie();
 }
 
 void testChart1() {
   final w = new app.Win(ap.desktop)
-    ..setTitle('Charts')..render(600, 600);
+    ..setTitle('Chart')..render(600, 600);
 
   new chart.Chart(w.getContent())
     ..addData([
@@ -33,7 +34,7 @@ void testChart1() {
 
 void testChart() {
   final w = new app.Win(ap.desktop)
-    ..setTitle('Charts')..render(600, 600);
+    ..setTitle('Chart Bar')..render(600, 600);
 
   new chart.Bar(w.getContent())
     ..setData([
@@ -49,7 +50,7 @@ void testChart() {
 
 void testCircle() {
   final w = new app.Win(ap.desktop)
-   ..setTitle('Charts')..render(350, 350);
+   ..setTitle('Circle')..render(350, 350);
 
   new chart.Circle(w.getContent())
    ..setData(75)..draw();
@@ -57,9 +58,23 @@ void testCircle() {
 
 void testBarSmall() {
   final w = new app.Win(ap.desktop)
-   ..setTitle('Charts')..render(350, 350);
+   ..setTitle('Bar small')..render(350, 350);
 
   w.getContent()
     ..setStyle({'padding': '50px'})
     ..append(new chart.BarSmall(100)..setPercents(20));
+}
+
+void testPie() {
+  final w = new app.Win(ap.desktop)
+    ..setTitle('Pie')..render(600, 600);
+
+  new chart.Pie(w.getContent())
+    ..setData([
+      ['Иван Иванов', 23141],
+      ['Димитър Димитров', 10000],
+      ['Петър Събев', 31314],
+      ['Христо Балабанов', 1333],
+      ['Александър Александров', 200]])
+    ..redraw();
 }
