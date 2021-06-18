2e21358f7183932eff3fbba4193b5eb1ffa755b9
Petar Sabev <pisabev@gmail.com>
2021-06-18 16:16:43 +0300

    .

--- lib/src/db/schema/create.sql
+++ lib/src/db/schema/create.sql
@@ -63,12 +63,7 @@ CREATE OR REPLACE FUNCTION base_audit_trig()
 AS
 $function$
 BEGIN
-    IF TG_OP = 'INSERT'
-    THEN
-        INSERT INTO base_audit (table_name, operation, after)
-        VALUES (TG_TABLE_NAME, TG_OP, to_jsonb(NEW));
-        RETURN NEW;
-    ELSIF TG_OP = 'UPDATE'
+    IF TG_OP = 'UPDATE'
     THEN
         IF NEW != OLD THEN
             INSERT INTO base_audit (table_name, operation, before, after)
@@ -85,7 +80,7 @@ END;
 $function$;
 
 -- CREATE TRIGGER audit_[table]
---     BEFORE INSERT OR UPDATE OR DELETE
+--     BEFORE UPDATE OR DELETE
 --     ON [table]
 --     FOR EACH ROW
 -- EXECUTE PROCEDURE base_audit_trig();
--- lib/src/db/schema/patch/1.2021-06-18.sql
+++ lib/src/db/schema/patch/1.2021-06-18.sql
@@ -16,12 +16,7 @@ CREATE OR REPLACE FUNCTION base_audit_trig()
 AS
 $function$
 BEGIN
-    IF TG_OP = 'INSERT'
-    THEN
-        INSERT INTO base_audit (table_name, operation, after)
-        VALUES (TG_TABLE_NAME, TG_OP, to_jsonb(NEW));
-        RETURN NEW;
-    ELSIF TG_OP = 'UPDATE'
+    IF TG_OP = 'UPDATE'
     THEN
         IF NEW != OLD THEN
             INSERT INTO base_audit (table_name, operation, before, after)
